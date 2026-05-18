72aaa2ce034f3822ea0e0c0cecb7b69b47a7b209
michalwa <michalwa2003@gmail.com>
2026-05-19 01:31:49 +0200

    benchmark removals

--- src/lib.rs
+++ src/lib.rs
@@ -345,4 +345,47 @@ mod benches {
         populate_store(&mut store);
         bench_insertion(b, store);
     }
+
+    fn bench_removal(b: &mut Bencher, mut store: impl Store<String, String>) {
+        let mut entry_ids = store.entry_ids().collect::<Vec<_>>();
+
+        b.iter(|| {
+            // FIXME: This should not be part of the benchmark, but I don't know
+            // how to otherwise ensure there are always entries to remove
+            if entry_ids.is_empty() {
+                populate_store(&mut store);
+                entry_ids = store.entry_ids().collect::<Vec<_>>();
+            }
+
+            let (i, &entry) = entry_ids
+                .iter()
+                .enumerate()
+                .choose(&mut rand::rng())
+                .unwrap();
+
+            store.remove_entry(entry);
+            entry_ids.swap_remove(i);
+        });
+    }
+
+    #[bench]
+    fn naive_remove(b: &mut Bencher) {
+        let mut store = NaiveStore::default();
+        populate_store(&mut store);
+        bench_removal(b, store);
+    }
+
+    #[bench]
+    fn cross_linked_remove(b: &mut Bencher) {
+        let mut store = CrossLinkedStore::default();
+        populate_store(&mut store);
+        bench_removal(b, store);
+    }
+
+    #[bench]
+    fn semi_linked_remove(b: &mut Bencher) {
+        let mut store = SemiLinkedStore::default();
+        populate_store(&mut store);
+        bench_removal(b, store);
+    }
 }
